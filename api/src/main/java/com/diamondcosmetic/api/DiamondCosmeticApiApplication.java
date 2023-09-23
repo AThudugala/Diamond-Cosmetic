c3a31cf20a239f3051da593d7087ae7f595d4cd4
Anupa Thudugala <anuamawarni@gmail.com>
2023-09-23 15:50:26 +0530

    init project

---
+++ api/src/main/java/com/diamondcosmetic/api/DiamondCosmeticApiApplication.java
@@ -0,0 +1,13 @@
+package com.diamondcosmetic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondCosmeticApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondCosmeticApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/diamondcosmetic/api/DiamondCosmeticApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diamondcosmetic.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondCosmeticApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
