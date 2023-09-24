c8a323a0956bdeae74b3a7cb12dacb4b5cc64384
Anupa Thudugala <anuamawarni@gmail.com>
2023-09-24 13:32:12 +0530

    database configuration done

--- api/src/main/java/com/diamondcosmetic/api/model/Product.java
+++ api/src/main/java/com/diamondcosmetic/api/model/Product.java
@@ -1,4 +1,20 @@
 package com.diamondcosmetic.api.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @Id
+    @Column(name = "product_id", nullable = false)
+    private String product_id;
+    private String product_name;
+    private String product_category;
+    private String product_description;
+    private String directions;
+    private float price;
+    private int in_stock;
 }
